96bce54db77b08351c9ef22a63d03e7ef081fc0c
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-09-15 12:39:53 +0200

    Add styling to index.html and add spec tests

--- features/application.feature.js
+++ features/application.feature.js
@@ -21,4 +21,11 @@ context('Your Description of the test scenario', () => {
   it('renders the correct page title', async () => {
     expect(await browser.page.title()).to.eql('Puppeteer Mocha Scaffold');
   });
+
+  it('clicking on the "Check" button', async () => {
+    await browser.fillIn("input[id='value']", { with:  "3" });
+    await browser.clickOnButton("input[value='Check']");
+    let content = await browser.getContent("[id='display_answer']");
+    expect(content).to.eql('Fizz');
+  });
 });
